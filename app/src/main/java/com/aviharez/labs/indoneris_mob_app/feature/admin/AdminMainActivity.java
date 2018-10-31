a69fac771e1eeb3fb517ffb0bba9fafa2cad02c9
efendi19 <efen.ndi19@gmail.com>
2018-10-31 15:49:02 +0700

    adminmain

--- app/src/main/java/com/aviharez/labs/indoneris_mob_app/feature/admin/AdminMainActivity.java
+++ app/src/main/java/com/aviharez/labs/indoneris_mob_app/feature/admin/AdminMainActivity.java
@@ -8,10 +8,15 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.aviharez.labs.indoneris_mob_app.R;
 
+import java.util.ArrayList;
+
 public class AdminMainActivity extends AppCompatActivity {
+
+    private ArrayList<String> data = new ArrayList<String>();
 
     private BottomNavigationView bottomNavigationView;
     private Fragment fragment;
@@ -21,6 +26,10 @@ public class AdminMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+        ListView lv = (ListView) findViewById(R.id.listview);
+        generateListContent();
+        lv.setAdapter(new MyListAdapter(this, R.));
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.btnav);
         bottomNavigationView.inflateMenu(R.menu.admin_botnav_item);
