1ab85595c6e4a55e65820a4db398bfd607595985
aviharez <elzainsyifa@gmail.com>
2018-10-31 13:04:21 +0700

    Update gradle

--- app/src/main/java/com/aviharez/labs/indoneris_mob_app/feature/SplashScreen.java
+++ app/src/main/java/com/aviharez/labs/indoneris_mob_app/feature/SplashScreen.java
@@ -1,14 +1,31 @@
 package com.aviharez.labs.indoneris_mob_app.feature;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import com.aviharez.labs.indoneris_mob_app.R;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int splashInterval = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashInterval);
     }
 }
