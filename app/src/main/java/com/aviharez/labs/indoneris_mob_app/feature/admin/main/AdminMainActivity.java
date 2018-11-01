7336b15e710dd132bf4f978a353d18980bad59a2
efendi19 <efen.ndi19@gmail.com>
2018-11-01 16:36:16 +0700

    adminmain

--- app/src/main/java/com/aviharez/labs/indoneris_mob_app/feature/admin/main/AdminMainActivity.java
+++ app/src/main/java/com/aviharez/labs/indoneris_mob_app/feature/admin/main/AdminMainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,5 +72,11 @@ public class AdminMainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_main, menu);
+        return true;
     }
 }
